b8eb24c2f872f0af0bf5e9c212f7dd0fd8fd9f2d
owalker10 <36974651+owalker10@users.noreply.github.com>
2020-06-06 18:25:34 -0500

    Hopefully fixed submodule bug in oauth-server

---
+++ oauth-server/server.js
@@ -0,0 +1,52 @@
+let fs = require('fs')
+let express = require('express')
+let request = require('request')
+let querystring = require('querystring')
+
+
+let app = express()
+
+var {"client-id": SPOTIFY_CLIENT_ID, "client-secret": SPOTIFY_CLIENT_SECRET}
+  = JSON.parse(fs.readFileSync('secret.json'))
+
+
+let redirect_uri = 
+  process.env.REDIRECT_URI || 
+  'http://localhost:8888/callback'
+
+app.get('/login', function(req, res) {
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: SPOTIFY_CLIENT_ID,
+      scope: 'user-read-private user-read-email',
+      redirect_uri
+    }))
+})
+
+app.get('/callback', function(req, res) {
+  let code = req.query.code || null
+  let authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(
+        SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET
+      ).toString('base64'))
+    },
+    json: true
+  }
+  request.post(authOptions, function(error, response, body) {
+    var access_token = body.access_token
+    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+    res.redirect(uri + '?access_token=' + access_token)
+  })
+})
+
+let port = process.env.PORT || 8888
+console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`)
+app.listen(port)
